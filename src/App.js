7eaf6364e3465c289fc31e7c56e95f9435bb7d7f
Ayesha W <ayesha.wan@youdo.co.nz>
2018-10-11 14:32:06 +1300

    Init setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <h1>Hello World</h1>
+      <h1>Hello, {this.props.name}</h1>
     );
   }
 }
